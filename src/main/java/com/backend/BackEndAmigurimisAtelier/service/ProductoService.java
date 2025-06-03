67e712dec48ee666c8b9b76f329af66e36654130
andres1988 <a.canorave@gmail.com>
2025-06-03 10:36:19 -0500

    FEAT:iImplementar metodos del jpa en servicio

--- src/main/java/com/backend/BackEndAmigurimisAtelier/service/ProductoService.java
+++ src/main/java/com/backend/BackEndAmigurimisAtelier/service/ProductoService.java
@@ -1,8 +1,35 @@
 package com.backend.BackEndAmigurimisAtelier.service;
 
+import com.backend.BackEndAmigurimisAtelier.model.Producto;
 import com.backend.BackEndAmigurimisAtelier.serviceInterface.IProductoService;
+
+import java.util.List;
 
 
 public class ProductoService implements IProductoService {
 
+    @Override
+    public List<Producto> getAllPosts() {
+        return List.of();
+    }
+
+    @Override
+    public Producto getPostById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void saveProducto(Producto producto) {
+
+    }
+
+    @Override
+    public void deleteProducto(Long Id) {
+
+    }
+
+    @Override
+    public void editProducto(Long id, Producto productoActualizado) {
+
+    }
 }
